1e28215981833dc2d551e3f2deb2b942672c76bb
shubham-sde-null <shubhamsdecpporigin@gmail.com>
2023-02-05 21:01:00 +0530

    small changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,6 +10,7 @@ function Header() {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Shubham</h1>
+        {/* <h5 className="text-light">Full Stack Developer</h5> */}
         <h5 className="text-light">Full Stack Developer & Software Engineer</h5>
         <CTA />
         <HeaderSocials />
